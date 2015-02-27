47d0cccbe621597a048216e4c1aa1daaef75613d
lei <kmdjs@qq.com>
2015-02-27 20:33:38 +0800

    fix bug

---
+++ doc/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bitmap",
+        "Container",
+        "DisplayObject",
+        "Dom",
+        "FPS",
+        "Loader",
+        "Particle",
+        "ParticleSystem",
+        "RAF",
+        "Shape",
+        "Sprite",
+        "Stage",
+        "TWEEN",
+        "Txt",
+        "Vector2"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
